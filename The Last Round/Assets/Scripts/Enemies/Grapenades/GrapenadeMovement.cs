eee6960e23f3aa4dd1271528d267c1199fa1e1e2
Cucurru <arguerir@gmail.com>
2025-02-23 21:32:50 +0100

    Movimiento del Grapenade

---
+++ The Last Round/Assets/Scripts/Enemies/Grapenades/GrapenadeMovement.cs
@@ -0,0 +1,93 @@
+//---------------------------------------------------------
+// Contiene el movimiento del enemigo "Grapenade"
+// Aryan Guerrero Iruela
+// The Last Round
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Esta clase se encarga del movimiento del enemigo Grapenade
+/// Perseguirá al jugador hasta una cierta distancia, donde se quedará quieto. 
+/// Y si esta distancia es reducida, se moverá en sentido contrario hasta que vuelva a estar en esa distancia
+/// </summary>
+public class GrapenadeMovement : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    [SerializeField]
+    private float range;
+    [SerializeField]
+    private float marchaAtrasSpeed;
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    private Rigidbody2D rb;
+    private MoveToPlayer moveToPlayer;
+    private Vector3 EnemyPlayer;
+    #endregion
+    
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+    
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+    
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        moveToPlayer = GetComponent<MoveToPlayer>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void FixedUpdate()
+    {
+        EnemyPlayer = moveToPlayer.UpdateVector(gameObject);
+
+        if (Mathf.Floor(EnemyPlayer.magnitude*10)/10 == range)
+        {
+            rb.velocity = Vector3.zero;
+        }
+        else if(Mathf.Floor(EnemyPlayer.magnitude*10)/10 < range)
+        {
+            rb.velocity = -EnemyPlayer.normalized * marchaAtrasSpeed * Time.deltaTime;
+        }
+        else
+        {
+            moveToPlayer.Move(gameObject);
+        }
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion
+
+} // class Grapenade 
+// namespace
