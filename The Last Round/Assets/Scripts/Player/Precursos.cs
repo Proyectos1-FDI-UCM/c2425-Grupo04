13035abe7da20dcc6e2cf255065eb1b583f19dad
letianliye <3119889142lj@gmail.com>
2025-03-01 00:01:49 +0100

    Update Precursos.cs

--- The Last Round/Assets/Scripts/Player/Precursos.cs
+++ The Last Round/Assets/Scripts/Player/Precursos.cs
@@ -1,6 +1,6 @@
 //---------------------------------------------------------
-// Breve descripción del contenido del archivo
-// Responsable de la creación de este archivo
+// Este sirve para destruir los recursos cuand oel jugador toca con los recursos
+// Letian LIye
 // The Last Round
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
