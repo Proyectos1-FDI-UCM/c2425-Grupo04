868b06d43cfa35aa172971c1b62364f5ffd7b7b2
Cucurru <arguerir@gmail.com>
2025-03-03 16:02:17 +0100

    lo que llevo hecho de lo de las opciones
    En el UIManager lo que he añadido esta con es comentario vacio al lado. Y en vez de editar tu codigo, estaba haciendo lo de las opciones en un nuevo script llamado Player Dialogue

---
+++ The Last Round/Assets/Scripts/Clients/Player Dialogue.cs
@@ -0,0 +1,96 @@
+//---------------------------------------------------------
+// Breve descripción del contenido del archivo
+// Responsable de la creación de este archivo
+// The Last Round
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Dialogos y sus opciones de dialogo del jugador
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class PlayerDialogue : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+    [SerializeField, TextArea(5, 6)]
+    string[] dialogue01, dialogue02;
+    [SerializeField]
+    string[] option01, option02;
+    [SerializeField]
+    int[] correcta; //0 es opcion1, 1 es opcion2
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+    private UIManager uiManager;
+    private int tmp;
+    #endregion
+
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+        tmp = Random.Range(0, 2);
+    }
+
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    public void Update()
+    {
+        if (uiManager == null) uiManager = GameManager.Instance.GetUI();
+        if (uiManager.chequeoOpciones())
+        {
+            uiManager.SetChoice(option01[tmp], option02[tmp], dialogue01[tmp], dialogue02[tmp], correcta[tmp]);
+        }
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion   
+
+} // class DialogueOptions 
+// namespace
--- The Last Round/Assets/Scripts/Managers/UIManager.cs
+++ The Last Round/Assets/Scripts/Managers/UIManager.cs
@@ -23,11 +23,13 @@ public class UIManager : MonoBehaviour
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
     [SerializeField]
-    TextMeshProUGUI dialogueBox, Button;
+    TextMeshProUGUI dialogueBox, Button, option1Text, option2Text;
     [SerializeField]
     Button ButtonBox;
     [SerializeField]
     private float TypeSpeed;
+    [SerializeField]
+    private Button option1Box, option2Box;
     #endregion
 
     // ---- ATRIBUTOS PRIVADOS ----
@@ -35,7 +37,9 @@ public class UIManager : MonoBehaviour
     private string[,] dialogue;
     private int way = 1; // 1 = good way , 2 = bad way
     private int DialogueLine = 0;
-    private bool SkipDialogue = false;
+    private bool SkipDialogue = false, 
+        hayOpciones = false; //
+    private int correcta; //
     #endregion
 
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
@@ -53,6 +57,8 @@ public class UIManager : MonoBehaviour
     {
         GameManager.Instance.GiveUI(this);
         ButtonBox.gameObject.SetActive(true);
+        option1Box.gameObject.SetActive(false);//
+        option2Box.gameObject.SetActive(false);//
     }
 
     /// <summary>
@@ -60,7 +66,7 @@ public class UIManager : MonoBehaviour
     /// </summary>
     void Update()
     {
-        if (dialogue != null && dialogue[DialogueLine, 0] == dialogueBox.text) Button.text = "Continuar";
+        if (dialogue != null && dialogue[DialogueLine, way] == dialogueBox.text) Button.text = "Continuar";
         else Button.text = "Saltar";
     }
     #endregion
@@ -76,7 +82,7 @@ public class UIManager : MonoBehaviour
 
     public void SkipButton()
     {
-        if (dialogue != null && dialogue[DialogueLine, 0] == dialogueBox.text)
+        if (dialogue != null && dialogue[DialogueLine, way] == dialogueBox.text)
         {
             if (DialogueLine < dialogue.GetLength(0) - 1)
             {
@@ -85,6 +91,7 @@ public class UIManager : MonoBehaviour
             }
             else
             {
+                HayOpciones();//
                 ButtonBox.gameObject.SetActive(false);
                 dialogueBox.text = " ";
             }
@@ -95,6 +102,70 @@ public class UIManager : MonoBehaviour
             SkipDialogue = true;
         }
     }
+
+    public void SetChoice(string option1, string option2, string dialogue1, string dialogue2, int correcta) //
+    {
+        option1Text.text = option1;
+        option1Box.gameObject.SetActive(true);
+        option2Text.text = option2;
+        option2Box.gameObject.SetActive(true);
+        dialogue[0, 0] = dialogue1;
+        dialogue[1, 0] = dialogue2;
+        this.correcta = correcta;
+    }
+
+    public void OptionL() //
+    {
+        option1Box.gameObject.SetActive(false);
+        option2Box.gameObject.SetActive(false);
+        DialogueLine = 0;
+        way = 0;
+        StartCoroutine(Write());
+
+        if (correcta != 0)
+        {
+            way = 1;
+        }
+        else
+        {
+            way = 0;
+        }
+    }
+    public void OptionR() //
+    {
+        option1Box.gameObject.SetActive(false);
+        option2Box.gameObject.SetActive(false);
+        DialogueLine = 1;
+        way = 0;
+        StartCoroutine(Write());
+
+        if (correcta != 1)
+        {
+            way = 1;
+        }
+        else
+        {
+            way = 0;
+        }
+    }
+    public void HayOpciones() //Decide al azar si hay opciones o no //
+    {
+        int rnd = Random.Range(0, 2);
+        if (rnd == 0)
+        {
+            hayOpciones = false;
+        }
+        else
+        {
+            hayOpciones = true;
+        }
+    }
+
+    public bool chequeoOpciones()//
+    {
+        return hayOpciones;
+    }
+
     #endregion
 
     // ---- MÉTODOS PRIVADOS ----
@@ -103,19 +174,20 @@ public class UIManager : MonoBehaviour
     {
         dialogueBox.text = string.Empty;
 
-        for (int i = 0; i < dialogue[DialogueLine, 0].Length; i++)
+        for (int i = 0; i < dialogue[DialogueLine, way].Length; i++)
         {
-            char ch = dialogue[DialogueLine, 0][i];
+            char ch = dialogue[DialogueLine, way][i];
             if (SkipDialogue)
             {
-                dialogueBox.text = dialogue[DialogueLine, 0];
-                i = dialogue[DialogueLine, 0].Length;
+                dialogueBox.text = dialogue[DialogueLine, way];
+                i = dialogue[DialogueLine, way].Length;
                 SkipDialogue = false;
             }
             else dialogueBox.text += ch;
             yield return new WaitForSeconds(TypeSpeed);
         }
     }
+
     #endregion
 
 } // class UIManager 
