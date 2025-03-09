fec019c8e0e7124eaebd96e0412fa11e8fa9b052
VictorCastro3 <viccas03@ucm.es>
2025-03-09 01:49:04 +0100

    Sistema de vida de jugador mayormente completo
    Digo mayormente porque falta lo del UIManager, lo hago mañana por la mañana :)

--- The Last Round/Assets/Scripts/Enemies/EnemyLife.cs
+++ The Last Round/Assets/Scripts/Enemies/EnemyLife.cs
@@ -1,6 +1,6 @@
 //---------------------------------------------------------
-// Breve descripción del contenido del archivo
-// Responsable de la creación de este archivo
+// Controla la vida del enemigo
+// Letian Liye
 // The Last Round
 // Proyectos 1 - Curso 2024-25
 //---------------------------------------------------------
---
+++ The Last Round/Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,83 @@
+//---------------------------------------------------------
+// Controlador de la vida del jugador
+// Víctor Castro Álvarez
+// The Last Round
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using UnityEngine;
+using TMPro;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class PlayerHealth : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    [SerializeField] int health = 50;
+    [SerializeField] GameObject ContadorDaño;
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    bool hasDied; 
+    private TextMeshProUGUI text;
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+    private void Start()
+    {
+        text = ContadorDaño.GetComponentInChildren<TextMeshProUGUI>();
+    
+        hasDied = false;
+    }
+
+    private void Update()
+    {
+        //Reemplazar con lo que sea después
+        if(health <= 0)
+        {
+            hasDied = true;
+        }
+
+        if (hasDied)
+        {
+            //Activar UI de muerte y volver a menú
+        }
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    public void HitPlayer(int damage)
+    {
+        health -= damage;
+        text.text = damage.ToString();
+        Instantiate(ContadorDaño, gameObject.transform.position, Quaternion.identity);
+    }
+
+    public bool hasPlayerDied()
+    {
+        return hasDied;
+    }
+
+    #endregion
+    
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+
+    #endregion   
+
+} // class PlayerHealth 
+// namespace
