2e02cade222b6968af167346034216ec56445c18
victoEnano <vicmar20@ucm.es>
2025-04-14 23:45:20 +0200

    No superar los límites con dash
    Ya no se puede usar el dash para salirse de los límites del mapa

--- The Last Round/Assets/Scripts/Player/PlayerDash.cs
+++ The Last Round/Assets/Scripts/Player/PlayerDash.cs
@@ -6,6 +6,7 @@
 //---------------------------------------------------------
 
 using UnityEngine;
+using UnityEngine.EventSystems;
 // Añadir aquí el resto de directivas using
 
 /// <summary>
@@ -17,17 +18,20 @@ public class PlayerDash : MonoBehaviour
     // ---- ATRIBUTOS DEL INSPECTOR ----
     #region Atributos del Inspector (serialized fields)
     [Header("DASH")]
-    public float DashSpeed = 10f;
-    public float DashDuration = 0.2f;
-    public float DashCooldown = 1f;
+    [SerializeField]
+    private float DashSpeed = 10f,
+                  DashDuration = 0.2f,
+                  DashCooldown = 1f;
+    
     #endregion
 
     // ---- ATRIBUTOS PRIVADOS ----
     #region Atributos Privados (private fields)
     private bool isDashing = false;
-    private float cooldownTimer = 0f;
+    private float cooldownTimer = 0f, minX, maxX, minY, maxY;
     private Vector3 LastDirection;
     private Rigidbody2D rb;
+    private Collider2D ObjectCollider;
     #endregion
 
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
@@ -44,6 +48,12 @@ public class PlayerDash : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        ObjectCollider = GetComponent<Collider2D>();
+        maxX = GameManager.Instance.GetMapWidth() / 2;
+        minX = -maxX;
+
+        maxY = GameManager.Instance.GetMapHeight() / 2;
+        minY = -maxY;
     }
 
     /// <summary>
@@ -54,7 +64,6 @@ public class PlayerDash : MonoBehaviour
         LastDirection = GetComponent<PlayerMovement>().GetLastDirection();
         if (InputManager.Instance.DashWasPressedThisFrame() && CanDash())
         {
-           
           StartDash();
         }
         if (cooldownTimer > 0) cooldownTimer -= Time.deltaTime;
@@ -66,6 +75,21 @@ public class PlayerDash : MonoBehaviour
         {
             if (DashDuration > 0)
             {
+                Vector3 posMinX, posMinY, posMaxX, posMaxY;
+
+                posMinX = rb.position - new Vector2(ObjectCollider.bounds.size.x / 2, 0);
+                posMaxX = rb.position + new Vector2(ObjectCollider.bounds.size.x / 2, 0);
+                posMinY = rb.position - new Vector2(0, ObjectCollider.bounds.size.y / 2);
+                posMaxY = rb.position + new Vector2(0, ObjectCollider.bounds.size.y / 2);
+
+                if ((rb.velocity.y < 0 && posMinY.y <= minY) ||
+                    (rb.velocity.y > 0 && posMaxY.y >= maxY))
+                    rb.velocity = new Vector2(rb.velocity.x, 0);
+
+                if ((rb.velocity.x < 0 && posMinX.x <= minX) ||
+                    (rb.velocity.x > 0 && posMaxX.x >= maxX))
+                    rb.velocity = new Vector2(0, rb.velocity.y);
+
                 DashDuration -= Time.fixedDeltaTime;
             }
             else
@@ -101,11 +125,9 @@ public class PlayerDash : MonoBehaviour
     }
     void StartDash()
     {
-        
         isDashing = true;
         cooldownTimer = DashCooldown;
         rb.velocity = new Vector3(LastDirection.x * DashSpeed, LastDirection.y * DashSpeed);
-      
     }
 
 
