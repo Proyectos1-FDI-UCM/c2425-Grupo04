4a77551191530af7f98b19039847045b1c0bcab5
Cucurru <arguerir@gmail.com>
2025-04-25 23:50:52 +0200

    Cheat de numero de enemigos

--- The Last Round/Assets/Scripts/Cheats.cs
+++ The Last Round/Assets/Scripts/Cheats.cs
@@ -88,10 +88,10 @@ public class Cheats : MonoBehaviour
 
             //numEnemigo v
             //Manejan los enemigos con los que empieza la partida
-            GameManager.Instance.SetNumEnemies(0, Mathf.RoundToInt(numManzurrias.value));
-            GameManager.Instance.SetNumEnemies(1, Mathf.RoundToInt(numManzarietes.value));
-            GameManager.Instance.SetNumEnemies(2, Mathf.RoundToInt(numGrapenades.value));
-            GameManager.Instance.SetNumEnemies(3, Mathf.RoundToInt(numUvoncios.value));
+            GameManager.Instance.SetNumEnemies(1, Mathf.RoundToInt(numManzurrias.value));
+            GameManager.Instance.SetNumEnemies(3, Mathf.RoundToInt(numManzarietes.value));
+            GameManager.Instance.SetNumEnemies(4, Mathf.RoundToInt(numGrapenades.value));
+            GameManager.Instance.SetNumEnemies(2, Mathf.RoundToInt(numUvoncios.value));
 
             //numHab para Grap y Manzariete v
             //Controlan el numero necesario de habitantes para que spawneen
