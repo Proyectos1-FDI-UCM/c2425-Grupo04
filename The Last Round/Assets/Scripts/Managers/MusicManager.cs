e267e4ab60f0442cb208e75dfa4ab960394ebb8d
OscarDaF <oscardaf@ucm.es>
2025-04-30 00:58:49 +0200

    Music manager y cambios en el GameManager

---
+++ The Last Round/Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,122 @@
+//---------------------------------------------------------
+// Manager a cargo de las distintas músicas del juego
+// Óscar Daniel Fernández Cabana
+// The Last Round
+// Proyectos 1 - Curso 2024-25
+//---------------------------------------------------------
+
+using System;
+using UnityEditor.Search;
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class MusicManager : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+    [SerializeField]
+    private AudioSource musicSource;
+    [SerializeField]
+    private AudioClip[] GameMusic; // 0 = Inicio , 1 = Menu , 2 = Combate , 3 = Bartender , 4 = Mejoras , 5 = Escena final , 6 = Creditos / Derrota
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    #endregion
+
+    private static MusicManager instance;
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    /// 
+
+
+    private void Awake()
+    {
+        if (instance !=  null)
+        {
+            DestroyImmediate(this.gameObject);
+        }
+
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        GameManager.Instance.GiveMusicManager(this);
+        
+        //musicSource.clip = GameMusic[0];
+        //musicSource.Play();
+
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+    #endregion
+
+    public void ChangeCurrentMusic(int MusicArrayIndex)
+    {
+        Debug.Log(MusicArrayIndex);
+        if (GameMusic[MusicArrayIndex] != null)
+        {
+            /*
+            musicSource.clip = GameMusic[MusicArrayIndex];
+            musicSource.Play();
+            */
+        }
+    }
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    #endregion   
+
+} // class MusicManager 
+// namespace
